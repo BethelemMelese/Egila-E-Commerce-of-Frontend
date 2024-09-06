2fa2c27c585f9118b6275fa908904341f4fd1289
Bethelem Melese <bethelem.m@ambftradingplc.com>
2024-09-07 01:28:25 +0300

    Update the front end appurl

--- src/appurl.tsx
+++ src/appurl.tsx
@@ -1,3 +1,3 @@
-export const appUrl = "https://egila-e-commerce-of-backend-v2n6.vercel.app/api/";
+export const appUrl = "https://egila-e-commerce-of-backend.vercel.app/api/";
 
 export const token = localStorage.getItem("token");
